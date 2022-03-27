398fd7770a1534c1bcf50de3ee205e80190eabd4
GrootSoldier <95124031+GrootSoldier@users.noreply.github.com>
2022-03-27 18:28:13 +0530

    elipcworkspace added

---
+++ MrPlumberworkspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/MrPlumberWebAPP/Plumber.jsp
@@ -0,0 +1,25 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Plumber INFO</title>
+</head>
+<body>
+<div>
+<form action="Addtest" method="get">
+name<input type="text" name="name" id="name"><br>
+lastname<input type="text" name="last" id="last"><br>
+username<input type="text" name="user" id="user"><br>
+password<input type="text" name="pass" id="pass"><br>
+address<input type="text" name="address" id="address"><br>
+pincode<input type="text" name="pincode" id="pincode"><br>
+aadhar<input type="text" name="aadhar" id="aadhar"><br>
+phone<input type="text" name="phone" id="phone"><br>
+Date<input type="date" name="date" id="date">
+<input type="submit" value="submit">
+</form>
+</div>
+</body>
+</html>
---
+++ MrPlumberworkspace/MrPlumberWebAPP/src/entity/PlumberServiceMapTbl.java
@@ -0,0 +1,85 @@
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.List;
+
+
+/**
+ * The persistent class for the plumber_service_map_tbl database table.
+ * 
+ */
+@Entity
+@Table(name="plumber_service_map_tbl")
+@NamedQuery(name="PlumberServiceMapTbl.findAll", query="SELECT p FROM PlumberServiceMapTbl p")
+public class PlumberServiceMapTbl implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name="plumber_service_map_id")
+	private int plumberServiceMapId;
+
+	//bi-directional many-to-one association to BookingServiceMapTbl
+	@OneToMany(mappedBy="plumberServiceMapTbl")
+	private List<BookingServiceMapTbl> bookingServiceMapTbls;
+
+	//bi-directional many-to-one association to PlumberInfoTbl
+	@ManyToOne
+	@JoinColumn(name="plumber_id")
+	private PlumberInfoTbl plumberInfoTbl;
+
+	//bi-directional many-to-one association to ServiceInfoTbl
+	@ManyToOne
+	@JoinColumn(name="service_id")
+	private ServiceInfoTbl serviceInfoTbl;
+
+	public PlumberServiceMapTbl() {
+	}
+
+	public int getPlumberServiceMapId() {
+		return this.plumberServiceMapId;
+	}
+
+	public void setPlumberServiceMapId(int plumberServiceMapId) {
+		this.plumberServiceMapId = plumberServiceMapId;
+	}
+
+	public List<BookingServiceMapTbl> getBookingServiceMapTbls() {
+		return this.bookingServiceMapTbls;
+	}
+
+	public void setBookingServiceMapTbls(List<BookingServiceMapTbl> bookingServiceMapTbls) {
+		this.bookingServiceMapTbls = bookingServiceMapTbls;
+	}
+
+	public BookingServiceMapTbl addBookingServiceMapTbl(BookingServiceMapTbl bookingServiceMapTbl) {
+		getBookingServiceMapTbls().add(bookingServiceMapTbl);
+		bookingServiceMapTbl.setPlumberServiceMapTbl(this);
+
+		return bookingServiceMapTbl;
+	}
+
+	public BookingServiceMapTbl removeBookingServiceMapTbl(BookingServiceMapTbl bookingServiceMapTbl) {
+		getBookingServiceMapTbls().remove(bookingServiceMapTbl);
+		bookingServiceMapTbl.setPlumberServiceMapTbl(null);
+
+		return bookingServiceMapTbl;
+	}
+
+	public PlumberInfoTbl getPlumberInfoTbl() {
+		return this.plumberInfoTbl;
+	}
+
+	public void setPlumberInfoTbl(PlumberInfoTbl plumberInfoTbl) {
+		this.plumberInfoTbl = plumberInfoTbl;
+	}
+
+	public ServiceInfoTbl getServiceInfoTbl() {
+		return this.serviceInfoTbl;
+	}
+
+	public void setServiceInfoTbl(ServiceInfoTbl serviceInfoTbl) {
+		this.serviceInfoTbl = serviceInfoTbl;
+	}
+
+}
